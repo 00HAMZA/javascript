3a04cdf640d938db1a60313cbe9b5302574e9191
HMIZOO <hamzalawi3@gmai.com>
2025-11-30 14:04:05 +0100

    nothing

--- data/jasmin-tests/moneytest.js
+++ data/jasmin-tests/moneytest.js
@@ -5,6 +5,7 @@ describe("tests suite : formatCurrency", () => {
   });
   it("works with 0", () => {
     expect(formatCurrency(0)).toEqual("0.00");
+    expect(formatCurrency(0)).toEqual("0.00");
   });
   it("round out to the nearest cents", () => {
     expect(formatCurrency(2000.5)).toEqual("20.01");
