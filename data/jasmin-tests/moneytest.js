de419325696421b5b4b8f716fcfcba6dfaac5e7d
HMIZOO <hamzalawi3@gmail.com>
2025-11-30 14:01:45 +0100

    remove default tests and add money test

---
+++ data/jasmin-tests/moneytest.js
@@ -0,0 +1,12 @@
+import { formatCurrency } from "../../scripts/utiles/money.js";
+describe("tests suite : formatCurrency", () => {
+  it("converts Cents into Dollars :", () => {
+    expect(formatCurrency(2094)).toEqual("20.94");
+  });
+  it("works with 0", () => {
+    expect(formatCurrency(0)).toEqual("0.00");
+  });
+  it("round out to the nearest cents", () => {
+    expect(formatCurrency(2000.5)).toEqual("20.01");
+  })
+});
