ab90430305bbc82690a582abced2822926d01761
HMIZOO <hamzalawi3@gmail.com>
2025-12-03 00:18:40 +0100

    create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { rerenderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import '../data/cartoop.js';
+import '../data/cartClass.js';
 
 
 rerenderOrderSummary();
